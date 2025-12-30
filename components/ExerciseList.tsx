3c49c97b5513e862bfca8f4ac5fd53b824406d95
ShawnCsj <csjshawn@gmail.com>
2025-12-30 15:19:57 +0800

    feat: refactor exercise list

--- components/ExerciseList.tsx
+++ components/ExerciseList.tsx
@@ -1,14 +1,15 @@
 import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import React from "react";
-import { useRouter } from "expo-router";
+import { Router, useRouter } from "expo-router";
 import { Image } from "expo-image";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import Animated, { FadeInDown } from "react-native-reanimated";
+import { BodyPartExercise } from "@/types";
 
-export default function ExerciseList({ data }) {
+export default function ExerciseList({ data }: { data: BodyPartExercise[] }) {
   const router = useRouter();
   return (
     <View>
@@ -27,7 +28,15 @@ export default function ExerciseList({ data }) {
   );
 }
 
-const ExerciseCard = ({ item, router, index }) => {
+const ExerciseCard = ({
+  item,
+  router,
+  index,
+}: {
+  item: BodyPartExercise;
+  router: Router;
+  index: number;
+}) => {
   return (
     <Animated.View
       entering={FadeInDown.duration(400)
@@ -40,24 +49,23 @@ const ExerciseCard = ({ item, router, index }) => {
         }
         className="flex py-3 space-y-2"
       >
-        <View className="bg-neutral-200 shadow rounded-[25px] mb-2">
+        <View className="bg-neutral-200 shadow rounded-[10px] mb-2">
           <Image
             source={{ uri: item.gifUrl }}
             contentFit="cover"
             style={{
-              width: wp(44),
+              width: wp(45),
               height: wp(52),
-              borderRadius: 25,
+              borderRadius: 10,
             }}
-            className="rounded-[25px]"
           />
         </View>
 
         <Text
-          style={{ fontSize: hp(1.7) }}
+          style={{ fontSize: hp(1.7), width: wp(44), flexWrap: "wrap" }}
           className="text-neutral-700 font-semibold ml-1 tracking-wide capitalize"
         >
-          {item?.name.length > 20 ? item.name.slice(0, 25) + "..." : item.name}
+          {item.name}
         </Text>
       </TouchableOpacity>
     </Animated.View>
