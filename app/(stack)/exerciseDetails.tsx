b7eb9717b18ecc5dc500995b485bb71e185f3a5c
ShawnCsj <csjshawn@gmail.com>
2025-12-30 15:20:08 +0800

    feat: refactor exercise detail pg

--- app/(stack)/exerciseDetails.tsx
+++ app/(stack)/exerciseDetails.tsx
@@ -1,106 +1,103 @@
-import { View, Text, TouchableOpacity, ScrollView } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
-import { useLocalSearchParams, useRouter } from "expo-router";
+import { useLocalSearchParams } from "expo-router";
 import { Image } from "expo-image";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
-import Anticons from "react-native-vector-icons/AntDesign";
-import Animated, { FadeInDown } from "react-native-reanimated";
+import { SafeAreaView } from "react-native-safe-area-context";
+import BackIcon from "@/components/common/BackIcon";
+import { BodyPartExercise } from "@/types";
 
 export default function exerciseDetails() {
-  const item = useLocalSearchParams();
-  const router = useRouter();
+  const item: BodyPartExercise = useLocalSearchParams();
+  const secondaryMuscles = Array.isArray(item.secondaryMuscles)
+    ? item.secondaryMuscles
+    : item.secondaryMuscles.split(",");
+  const instructions = Array.isArray(item.instructions)
+    ? item.instructions
+    : item.instructions
+        .split(".,")
+        .map((i) => i.trim())
+        .filter(Boolean);
 
   return (
-    <View className="flex flex-1">
-      <View className="shadow-md bg-neutral-200 rounded-b-[40px]">
-        <Image
-          source={{ uri: item.gifUrl }}
-          contentFit="cover"
-          style={{ width: wp(100), height: wp(100) }}
-        />
+    <SafeAreaView className="bg-white">
+      <View>
+        <View className=" bg-neutral-200 rounded-b-[40px]">
+          <Image
+            source={{ uri: item.gifUrl }}
+            contentFit="cover"
+            style={{ width: wp(100), height: wp(100) }}
+          />
+        </View>
+
+        <BackIcon />
+
+        <ScrollView
+          className="mx-4 space-y-2 mt-3"
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 60 }}
+        >
+          <Text
+            style={{ fontSize: hp(3.5) }}
+            className="font-semibold text-neutral-800 tracking-wide capitalize mb-3"
+          >
+            {item.name}
+          </Text>
+
+          <Text
+            style={{ fontSize: hp(2) }}
+            className="text-neutral-700 tracking-wide mb-2"
+          >
+            Equipment:{" "}
+            <Text className="font-bold text-neutral-800 capitalize">
+              {item?.equipment}
+            </Text>
+          </Text>
+
+          <Text
+            style={{ fontSize: hp(2) }}
+            className="text-neutral-700 tracking-wide mb-2"
+          >
+            Secondary muscles:{" "}
+            <Text className="font-bold text-neutral-800 capitalize">
+              {secondaryMuscles.join(", ")}
+            </Text>
+          </Text>
+
+          <Text
+            style={{ fontSize: hp(2) }}
+            className="text-neutral-700 tracking-wide mb-3"
+          >
+            Target:{" "}
+            <Text className="font-bold text-neutral-800 capitalize">
+              {item?.target}
+            </Text>
+          </Text>
+
+          <Text
+            style={{ fontSize: hp(3) }}
+            className="font-semibold text-neutral-800 tracking-wide mb-3"
+          >
+            Instructions
+          </Text>
+
+          {instructions.map((instruction, idx) => {
+            return (
+              <Text
+                key={idx}
+                style={{ fontSize: hp(2) }}
+                className="text-neutral-800 mb-2"
+              >
+                {instruction}
+                {idx !== instructions.length - 1 ? "." : ""}
+              </Text>
+            );
+          })}
+        </ScrollView>
       </View>
-
-      <TouchableOpacity
-        onPress={() => router.back()}
-        className="mx-2 absolute rounded-full mt-10 mx-4 right-0"
-      >
-        <Anticons name="closecircle" size={hp(4.5)} color="#f43f5e" />
-      </TouchableOpacity>
-
-      <ScrollView
-        className="mx-4 space-y-2 mt-3"
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ paddingBottom: 60 }}
-      >
-        <Animated.Text
-          entering={FadeInDown.duration(300).springify()}
-          style={{ fontSize: hp(3.5) }}
-          className="font-semibold text-neutral-800 tracking-wide capitalize mb-3"
-        >
-          {item.name}
-        </Animated.Text>
-        <Animated.Text
-          entering={FadeInDown.delay(100).duration(300).springify()}
-          style={{ fontSize: hp(2) }}
-          className="text-neutral-700 tracking-wide mb-2"
-        >
-          Equipment:{" "}
-          <Text className="font-bold text-neutral-800 capitalize">
-            {item?.equipment}
-          </Text>
-        </Animated.Text>
-        <Animated.Text
-          entering={FadeInDown.delay(200).duration(300).springify()}
-          style={{ fontSize: hp(2) }}
-          className="text-neutral-700 tracking-wide mb-2"
-        >
-          Secondary muscles:{" "}
-          <Text className="font-bold text-neutral-800 capitalize">
-            {item?.secondaryMuscles.split(",").map((item, index, arr) => {
-              return (
-                <Text key={index}>
-                  {item}
-                  {index + 1 === arr.length ? "" : ", "}
-                </Text>
-              );
-            })}
-          </Text>
-        </Animated.Text>
-        <Animated.Text
-          entering={FadeInDown.delay(300).duration(300).springify()}
-          style={{ fontSize: hp(2) }}
-          className="text-neutral-700 tracking-wide mb-3"
-        >
-          Target:{" "}
-          <Text className="font-bold text-neutral-800 capitalize">
-            {item?.target}
-          </Text>
-        </Animated.Text>
-        <Animated.Text
-          entering={FadeInDown.delay(400).duration(300).springify()}
-          style={{ fontSize: hp(3) }}
-          className="font-semibold text-neutral-800 tracking-wide mb-3"
-        >
-          Instructions
-        </Animated.Text>
-        {item.instructions.split(".,").map((instruction, index) => {
-          return (
-            <Animated.Text
-              entering={FadeInDown.delay((index + 6) * 100)
-                .duration(300)
-                .springify()}
-              key={instruction}
-              style={{ fontSize: hp(1.7) }}
-              className="text-neutral-800 mb-2"
-            >
-              {instruction}.
-            </Animated.Text>
-          );
-        })}
-      </ScrollView>
-    </View>
+    </SafeAreaView>
   );
 }
