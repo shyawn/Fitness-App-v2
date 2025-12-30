30df534360c13848a9ac790d26df4e50beb91f69
ShawnCsj <csjshawn@gmail.com>
2025-12-31 01:47:12 +0800

    feat: refactor draggableList

--- components/DraggableList.tsx
+++ components/DraggableList.tsx
@@ -5,22 +5,30 @@ import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
-import Ionicons from "react-native-vector-icons/Ionicons";
+import Ionicons from "@react-native-vector-icons/ionicons";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../store/store";
-import {
-  deleteWorkout,
-  setWorkoutOrder,
-} from "@/store/workoutPlan/workoutSlice";
+import { deleteWorkout } from "@/store/workoutPlan/workoutSlice";
+import { useRouter } from "expo-router";
+
+interface DraggableProps {
+  selectedDay: string;
+  data: Workout[];
+  editModalVisible: boolean;
+  setEditModalVisible: (visible: boolean) => void;
+  setEditWorkout: (workout: Workout) => void;
+  onReordered: (updatedData: Workout[]) => void;
+}
 
 export default function DraggableList({
   selectedDay,
   editModalVisible,
   setEditModalVisible,
   setEditWorkout,
-}) {
+  onReordered,
+}: DraggableProps) {
   const dispatch = useDispatch<AppDispatch>();
-
+  const router = useRouter();
   const workoutList = useSelector((state: RootState) => state.workout);
 
   function keyExtractor(item: Workout) {
@@ -31,62 +39,69 @@ export default function DraggableList({
     setEditModalVisible(!editModalVisible);
   };
 
+  // Only render workout with selected day
+  const filteredWorkouts = workoutList.filter((item) =>
+    item.day.includes(selectedDay)
+  );
+
   function renderItem(info: DragListRenderItemInfo<Workout>) {
     const { item, onDragStart, onDragEnd } = info;
 
-    // Only render workout with selected day
-    if (item.day.includes(selectedDay)) {
-      return (
-        <TouchableOpacity
-          key={item.id}
-          onPressIn={onDragStart}
-          onPressOut={onDragEnd}
-          className="w-80 p-3 border-[1px] border-[#A9A9A9] rounded-lg flex flex-row justify-between items-center"
-        >
-          <View className="flex">
-            <Text className="font-semibold text-[15px] capitalize">
-              {item?.name.length > 20
-                ? item.name.slice(0, 20) + "..."
-                : item.name}
-            </Text>
-            <Text className="text-[13px] text-[#636363]">
-              {item?.sets} x {item?.weight} {item.weight ? "kg" : "sets"}
-            </Text>
-          </View>
+    return (
+      <TouchableOpacity
+        key={item.id}
+        onPressIn={onDragStart}
+        onPressOut={onDragEnd}
+        className="w-full my-1 p-3 border-[1px] bg-white border-[#A9A9A9] rounded-lg flex flex-row justify-between items-center"
+      >
+        <View className="flex">
+          <Text className="font-semibold text-[16px] capitalize mb-1">
+            {item.name}
+          </Text>
+          <Text className="text-[#636363]">
+            {item.sets} x {item.reps} reps ({item.weight}
+            {item.weight && " "}
+            {item.weight ? "kg" : "bodyweight"})
+          </Text>
+        </View>
 
-          <View className="flex flex-row gap-2">
-            {/* Opens edit modal */}
-            <TouchableOpacity onPress={() => toggleEditModal(item)}>
-              <Ionicons name="create" size={wp(6)} color="gray" />
-            </TouchableOpacity>
+        <View className="flex flex-row gap-4">
+          {/* Opens edit modal */}
+          <TouchableOpacity
+            style={{ height: hp(4), width: hp(4) }}
+            className="bg-[#999] rounded-full items-center justify-center"
+            onPress={() => toggleEditModal(item)}
+          >
+            <Ionicons name="pencil" size={wp(4)} color="white" />
+          </TouchableOpacity>
 
-            <TouchableOpacity
-              className="bg-rose-500 rounded-full p-[1px]"
-              onPress={() => {
-                dispatch(deleteWorkout(item));
-              }}
-            >
-              <Ionicons name="remove-outline" size={hp(3)} color="white" />
-            </TouchableOpacity>
-          </View>
-        </TouchableOpacity>
-      );
-    }
+          <TouchableOpacity
+            style={{ height: hp(4), width: hp(4) }}
+            className="bg-rose-500 rounded-full items-center justify-center"
+            onPress={() => {
+              dispatch(deleteWorkout(item));
+            }}
+          >
+            <Ionicons name="trash" size={hp(2.5)} color="white" />
+          </TouchableOpacity>
+        </View>
+      </TouchableOpacity>
+    );
   }
 
-  function onReordered(fromIndex: number, toIndex: number) {
+  function reorderWorkout(fromIndex: number, toIndex: number) {
     const reorderedList = [...workoutList]; // Make a copy of store data instead of modifying
     const [movedItem] = reorderedList.splice(fromIndex, 1);
     reorderedList.splice(toIndex, 0, movedItem); // Insert movedItem at new position to reorder
-    dispatch(setWorkoutOrder(reorderedList));
+    onReordered(reorderedList);
   }
 
   return (
-    <View>
+    <View className="px-6">
       <DragList
-        data={workoutList}
+        data={filteredWorkouts}
         keyExtractor={(item) => keyExtractor(item)}
-        onReordered={onReordered}
+        onReordered={reorderWorkout}
         renderItem={renderItem}
       />
     </View>
