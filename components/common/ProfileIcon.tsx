0280be4053414528b0e311896096ee3406ead7ed
ShawnCsj <csjshawn@gmail.com>
2026-01-02 17:38:51 +0800

    chore: edit profile icon

--- components/common/ProfileIcon.tsx
+++ components/common/ProfileIcon.tsx
@@ -9,7 +9,7 @@ const ProfileIcon = () => {
       className="bg-neutral-200 rounded-full flex justify-center items-center border-[1px] border-neutral-300"
       style={{ height: hp(4.5), width: hp(4.5) }}
     >
-      <Ionicons name="person-sharp" size={hp(2.5)} color="gray" />
+      <Ionicons name="person-sharp" size={hp(2.5)} color="#404040" />
     </TouchableOpacity>
   );
 };
