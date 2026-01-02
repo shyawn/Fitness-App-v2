a6c08cdbf8284c20d55af8fc7beda73a8f2181a8
ShawnCsj <csjshawn@gmail.com>
2026-01-02 15:36:48 +0800

    feat: add hydration calc util

--- utils/index.ts
+++ utils/index.ts
@@ -5,3 +5,7 @@ export const calcMacrosFulfilled = (goal: number, tracked: number) => {
 export const calcMacrosLeft = (goal: number, tracked: number) => {
   return goal - tracked;
 };
+
+export const calcHydrationCup = (servingAmt: number) => {
+  return Math.floor(servingAmt / 250);
+};
