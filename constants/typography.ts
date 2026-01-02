6f3ff19a1a22db5c38753892a1b2ff5cfbfc1bad
ShawnCsj <csjshawn@gmail.com>
2026-01-02 14:50:59 +0800

    feat: create typography

---
+++ constants/typography.ts
@@ -0,0 +1,20 @@
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
+
+export const Typography = {
+  header: {
+    fontSize: hp(3.5),
+    color: "#404040",
+  },
+  subheader: {
+    fontSize: 18,
+    color: "#404040",
+  },
+  body: {
+    fontSize: 14,
+    color: "#404040",
+  },
+  smallBody: {
+    fontSize: 12,
+    color: "#404040",
+  },
+};
