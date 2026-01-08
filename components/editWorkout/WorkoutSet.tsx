e2b20545119bec1b8ee23839b39488d61a0c1e31
ShawnCsj <csjshawn@gmail.com>
2026-01-08 22:23:37 +0800

    feat: remove workoutInput from workoutSet

--- components/editWorkout/WorkoutSet.tsx
+++ components/editWorkout/WorkoutSet.tsx
@@ -1,59 +1,20 @@
 import { Workout } from "@/types";
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import { View, Text } from "react-native";
-import SelectWorkoutType from "./SelectWorkoutType";
-import WorkoutInput from "./WorkoutInput";
+import WorkoutSetComp from "./WorkoutSetComp";
 
 interface WorkoutSetProps {
   workout: Workout;
   error: boolean;
-  setWorkout: (workout: Workout) => void;
+  setWorkout: Dispatch<SetStateAction<Workout>>;
 }
 
 const WorkoutSet = ({ workout, error, setWorkout }: WorkoutSetProps) => {
   return (
     <View className="px-6 mb-4">
-      <Text className="mb-2 font-semibold text-[18px] text-gray-500">Set</Text>
+      <Text className="mb-2 font-semibold text-[18px] text-gray-500">Sets</Text>
 
-      <SelectWorkoutType
-        workout={workout}
-        error={error && workout.type === ""}
-        setWorkout={setWorkout}
-      />
-
-      <View className="flex flex-row justify-around mt-5 gap-5">
-        <WorkoutInput
-          style={{ flex: 1 }}
-          value={workout.sets}
-          placeholder="Sets"
-          rightLabel="Sets"
-          isWholeNumber={true}
-          error={error && workout.sets === ""}
-          onChangeText={(text) => setWorkout({ ...workout, sets: text })}
-        />
-
-        <WorkoutInput
-          style={{ flex: 1 }}
-          value={workout.reps}
-          placeholder="Reps"
-          rightLabel="Reps"
-          isWholeNumber={true}
-          error={error && workout.reps === ""}
-          onChangeText={(text) => setWorkout({ ...workout, reps: text })}
-        />
-      </View>
-
-      {workout.type !== "Bodyweight" && (
-        <View style={{ width: "47%", marginTop: 8 }}>
-          <WorkoutInput
-            value={workout.weight}
-            placeholder="Weight"
-            rightLabel="kg"
-            error={error && workout.weight === ""}
-            onChangeText={(text) => setWorkout({ ...workout, weight: text })}
-          />
-        </View>
-      )}
+      <WorkoutSetComp workout={workout} setWorkout={setWorkout} error={error} />
     </View>
   );
 };
