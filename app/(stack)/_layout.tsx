e7a199758ee959940d0a101e70c01c0fe36eef15
ShawnCsj <csjshawn@gmail.com>
2026-01-09 18:05:06 +0800

    feat: add stack.protected for signin pages

--- app/(stack)/_layout.tsx
+++ app/(stack)/_layout.tsx
@@ -1,11 +1,29 @@
+import { useAuth } from "@clerk/clerk-expo";
 import { Stack } from "expo-router";
+import { ActivityIndicator, View } from "react-native";
 
 export default function RootStack() {
+  const { isLoaded, isSignedIn, userId, sessionId, getToken } = useAuth();
+
+  if (!isLoaded) {
+    return (
+      <View className="flex-1 items-center justify-center">
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Protected guard={isSignedIn}>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack.Protected>
+
+      <Stack.Protected guard={!isSignedIn}>
+        <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+        <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+      </Stack.Protected>
+    </Stack>
   );
 }
